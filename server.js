7c27765e3a6c8b06130e745c94968dc66db80e8b
“Valentyna <glukhenko.valentyna@gmail.com>
2026-05-07 18:13:18 +0300

    version 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Denai Systems! CI/CD works! 1.1 version');
+  res.send('Hello from Denai Systems! CI/CD works! 2 version');
 });
 
 app.listen(port, () => {
